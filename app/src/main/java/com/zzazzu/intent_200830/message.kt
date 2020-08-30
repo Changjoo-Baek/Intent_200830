94155c5a260f00f4df5af0b1885a036c17677079
bucephalus41 <bucephalus41@gmail.com>
2020-08-30 14:21:42 +0900

    Initial commit

---
+++ app/src/main/java/com/zzazzu/intent_200830/message.kt
@@ -0,0 +1,11 @@
+package com.zzazzu.intent_200830
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class message : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message)
+    }
+}
