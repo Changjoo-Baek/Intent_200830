590938b9fb717fe6eed955737b7d12b1a4d680f9
bucephalus41 <bucephalus41@gmail.com>
2020-08-30 14:36:00 +0900

    전부 완성. 구조를 다시 봐야 할 필요가 있다.

--- app/src/main/java/com/zzazzu/intent_200830/MainActivity.kt
+++ app/src/main/java/com/zzazzu/intent_200830/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.zzazzu.intent_200830
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherActivityBtn.setOnClickListener {
+
+            val myIntent = Intent(this, other::class.java)
+            startActivity(myIntent)
+
+        }
+
+        sendMessageBtn.setOnClickListener {
+
+            val inputMessage = messageEdt.text.toString()
+
+            val myIntent = Intent(this,  message::class.java)
+
+            myIntent.putExtra("message", inputMessage)
+
+            startActivity(myIntent)
+        }
+
     }
 }
