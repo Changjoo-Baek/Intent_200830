c69972e2010474243aeb69ac1998114e21edc973
bucephalus41 <bucephalus41@gmail.com>
2020-09-19 10:25:46 +0900

    추가작업 실시

--- app/src/main/java/com/zzazzu/intent_200830/message.kt
+++ app/src/main/java/com/zzazzu/intent_200830/message.kt
@@ -8,8 +8,11 @@ class message : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
+// 화면이 만들어질 때
 
         val receivedMessage = intent.getStringExtra("message")
+
+
 
         messageTxt.text = receivedMessage
 
